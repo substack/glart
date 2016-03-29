9fc4f0fa5ce23eef3ca6b3434aaa693d09979f10
James Halliday <mail@substack.net>
2016-03-28 20:02:08 -0700

    multiple cubes

--- main.js
+++ main.js
@@ -1,57 +1,83 @@
 var regl = require('regl')()
 var mat4 = require('gl-mat4')
 var cubeMesh = require('cube-mesh')
-
 var mesh = cubeMesh(1)
-var cube = regl({
-  frag: `
-    precision mediump float;
-    varying vec3 pos;
-    void main () {
-      gl_FragColor = vec4(pos, 1);
-    }
-  `,
-  vert: `
-    uniform mat4 proj;
-    uniform mat4 model;
-    uniform mat4 view;
 
-    attribute vec3 position;
-    varying vec3 pos;
-
-    void main () {
-      gl_Position = proj*model*view * vec4(position, 1.0);
-      pos = position;
-    }
-  `,
-  attributes: {
-    position: regl.buffer(mesh.positions)
-  },
-  depthTest: true,
-  elements: regl.elements(mesh.cells),
-  uniforms: {
-    proj: regl.prop('proj'),
-    model: regl.prop('model'),
-    view: regl.prop('view')
-  }
-})
-
-var view = mat4.identity([])
-var model = mat4.identity([])
-mat4.translate(model, model, [0,-0.3,-1.7])
-mat4.rotateX(model, model, 0.5)
-mat4.translate(model, model, [0,0.3,0])
+var cubes = []
+for (var i = 0; i < 20; i++) {
+  cubes.push(createCube())
+}
 
 var proj = mat4.identity([])
+var view = mat4.identity([])
+mat4.translate(view, view, [0,0,-5])
 
 regl.frame(function (count) {
   regl.clear({
-    color: [1,0.5,0,1]
+    color: [0.1,0.05,0,1],
+    depth: 1
   })
-  cube({
+  var opts = {
     proj: mat4.perspective(proj, Math.PI/2,
       window.innerWidth/window.innerHeight, 0, 1e12),
-    model: model,
-    view: mat4.rotateY(view, view, 0.01)
-  })
+    view: view
+  }
+  for (var i = 0; i < cubes.length; i++) cubes[i](opts)
 })
+
+function createCube () {
+  var pos = [
+    1/(2*Math.random()-1),
+    1/(2*Math.random()-1),
+    1/(2*Math.random()-1)
+  ]
+  var rotY = 0.01/Math.random() * 0.01
+  var rotX = 0.01/Math.random() * 0.01
+
+  var model = mat4.identity([])
+  mat4.translate(model, model, pos)
+
+  var draw = regl({
+    frag: `
+      precision mediump float;
+      varying vec3 pos;
+      void main () {
+        gl_FragColor = vec4(
+          sin(pos.z+cos(pos.y)),
+          sin(pos.x),
+          cos(pos.y),
+          1
+        );
+      }
+    `,
+    vert: `
+      uniform mat4 proj;
+      uniform mat4 model;
+      uniform mat4 view;
+
+      attribute vec3 position;
+      varying vec3 pos;
+
+      void main () {
+        gl_Position = proj*model*view * vec4(position, 1.0);
+        pos = position;
+      }
+    `,
+    attributes: {
+      position: regl.buffer(mesh.positions)
+    },
+    elements: regl.elements(mesh.cells),
+    uniforms: {
+      proj: regl.prop('proj'),
+      model: regl.prop('model'),
+      view: regl.prop('view')
+    },
+    depthTest: true
+  })
+  return function (opts) {
+    mat4.rotateX(model, model, rotX)
+    mat4.rotateY(model, model, rotY)
+    opts.model = model
+    return draw(opts)
+  }
+}
