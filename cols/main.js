95c19fb5e2fd294d14aecc23e2e1011e4ecccead
substack <substack@9nTgtYmvW4HID6ayt6Icwc8WZxdifx5SlSKKIX/X/1g=.ed25519>
2016-08-14 02:14:53 -0700

    pyramid

--- cols/main.js
+++ cols/main.js
@@ -5,10 +5,11 @@ var regl = require('regl')()
 var normals = require('angle-normals')
 var camera = require('regl-camera')(regl, { center: [25,0,0] })
 var mat4 = require('gl-mat4')
-var mat0 = []
+var vec3 = require('gl-vec3')
+var mat0 = [], v0 = []
 
-var draw0 = fromMesh(mesh0)
-var draw1 = fromMesh(mesh1)
+var col0 = fromMesh(mesh0)
+var col1 = fromMesh(mesh1)
 
 var batch0 = []
 for (var i = 0; i < 10; i++) {
@@ -25,11 +26,17 @@ for (var i = 0; i < 10; i++) {
   )
 }
 
+var pyramid = fromPyramid({
+  positions: [[100,0,-100],[100,0,100],[-100,0,100],[-100,0,-100],[0,100,0]],
+  cells: [[0,1,4],[1,2,4],[2,3,4],[0,3,4],[0,1,2],[2,3,0]]
+})
+
 regl.frame(() => {
   regl.clear({ color: [0,0,0,1] })
   camera(() => {
-    draw0(batch0)
-    draw1(batch1)
+    col0(batch0)
+    col1(batch1)
+    pyramid({ location: [-500,0,0] })
   })
 })
 
@@ -37,19 +44,32 @@ function fromMesh (mesh) {
   return regl({
     frag: `
       precision mediump float;
-      varying vec3 vnormal;
+      varying vec3 vnormal, vpos;
+      varying float vtime;
+      vec3 hsl2rgb(in vec3 hsl) {
+        vec3 rgb = clamp(abs(mod(hsl.x*6.0+vec3(0.0,4.0,2.0),6.0)-3.0)-1.0,0.0,1.0);
+        return hsl.z+hsl.y*(rgb-0.5)*(1.0-abs(2.0*hsl.z-1.0));
+      }
       void main () {
-        gl_FragColor = vec4(abs(vnormal), 1.0);
+        vec3 c = abs(vnormal) * 0.3
+          + vec3(0.2 + sin(vtime + vpos.x/200.0 + vpos.z/300.0),1,1) * 0.3
+        ;
+        c.y = 1.0;
+        gl_FragColor = vec4(hsl2rgb(c), 1.0);
       }
     `,
     vert: `
       precision mediump float;
       uniform mat4 projection, view, model;
+      uniform float time;
       attribute vec3 position, normal;
-      varying vec3 vnormal;
+      varying vec3 vnormal, vpos;
+      varying float vtime;
       void main () {
         vnormal = normal;
+        vtime = time;
         gl_Position = projection * view * model * vec4(position, 1.0);
+        vpos = vec3(gl_Position);
       }
     `,
     attributes: {
@@ -64,6 +84,64 @@ function fromMesh (mesh) {
         mat4.rotateY(mat0, mat0, theta)
         return mat0
       },
+      time: (context) => context.time,
+      location: regl.prop('location')
+    },
+    elements: mesh.cells
+  })
+}
+
+function fromPyramid (mesh) {
+  return regl({
+    frag: `
+      precision mediump float;
+      varying vec3 vnormal, vpos;
+      varying float vtime;
+      vec3 hsl2rgb(in vec3 hsl) {
+        vec3 rgb = clamp(abs(mod(hsl.x*6.0+vec3(0.0,4.0,2.0),6.0)-3.0)-1.0,0.0,1.0);
+        return hsl.z+hsl.y*(rgb-0.5)*(1.0-abs(2.0*hsl.z-1.0));
+      }
+      void main () {
+        vec3 c = vec3(abs(
+          sin(vtime/20.0 + sin(vnormal * 6.2))
+          + sin(vtime + vpos.x/200.0 + vpos.z/300.0)
+        ) + vec3(0,0,2));
+        c.y = 1.0;
+        c.z = sin(length(vpos)/4.0) * 0.5;
+        gl_FragColor = vec4(hsl2rgb(c), 1.0);
+      }
+    `,
+    vert: `
+      precision mediump float;
+      uniform mat4 projection, view, model;
+      uniform float time;
+      attribute vec3 position, normal;
+      varying vec3 vnormal, vpos;
+      varying float vtime;
+      void main () {
+        vnormal = normal;
+        vtime = time;
+        gl_Position = projection * view * model * vec4(position, 1.0);
+        vpos = vec3(gl_Position);
+      }
+    `,
+    attributes: {
+      position: mesh.positions,
+      normal: normals(mesh.cells, mesh.positions)
+    },
+    uniforms: {
+      model: (context, props) => {
+        var speed = (1+Math.sin(context.time/8 + 16))/2
+        var elev = Math.pow(speed, 4) * 200
+        var theta = context.time * speed * 10
+        mat4.identity(mat0)
+        mat4.translate(mat0, mat0, props.location)
+        vec3.set(v0,0,elev,0)
+        mat4.translate(mat0, mat0, v0)
+        mat4.rotateY(mat0, mat0, theta)
+        return mat0
+      },
+      time: (context) => context.time,
       location: regl.prop('location')
     },
     elements: mesh.cells
