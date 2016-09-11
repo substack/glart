b6825cce319d4bc24cd74194bec911a3f7efcea8
substack <substack@9nTgtYmvW4HID6ayt6Icwc8WZxdifx5SlSKKIX/X/1g=.ed25519>
2016-09-11 19:10:16 -0400

    bouncy flame

--- candle/main.js
+++ candle/main.js
@@ -5,6 +5,7 @@ var surfaceNets = require('surface-nets')
 var ndarray = require('ndarray')
 var fill = require('ndarray-fill')
 var normals = require('angle-normals')
+var sphereMesh = require('sphere-mesh')
 
 var fs = require('fs')
 var snoise = fs.readFileSync(require.resolve('glsl-noise/simplex/3d.glsl'),'utf8')
@@ -23,7 +24,8 @@ var camera = require('regl-camera')(regl, {
 
 var draw = {
   candle: candle(regl),
-  base: base(regl)
+  base: base(regl),
+  flame: flame(regl)
 }
 
 regl.frame(function () {
@@ -31,6 +33,7 @@ regl.frame(function () {
   camera(function () {
     draw.candle()
     draw.base()
+    draw.flame()
   })
 })
 
@@ -39,6 +42,119 @@ function build (regl, res, f) {
   return surfaceNets(fill(data, function (i,j,k) {
     return f(i/63*2-1,j/63*2-1,k/63*2-1)
   }))
+}
+
+function flame (regl) {
+  var model = []
+  var mesh = sphereMesh(20,0.25)
+  return regl({
+    frag: `
+      precision mediump float;
+      ${snoise}
+      ${cnoise}
+      uniform float time;
+      varying vec3 vpos;
+      void main () {
+        float c = snoise(curlNoise(vpos-vec3(0,time*0.25,0)));
+        gl_FragColor = vec4(sqrt(vec3(1,0.8,0.5)*(c+1.0)),0.5);
+      }
+    `,
+    vert: `
+      precision mediump float;
+      uniform mat4 projection, view, model;
+      uniform float time;
+      attribute vec3 position;
+      varying vec3 vpos;
+      ${snoise}
+      void main () {
+        vec3 offset = vec3(0,-1.55,0);
+        float c = (snoise(position*2.0-vec3(0,time*1.0,0))*2.0-1.0)*0.1+0.5;
+        vpos = (position - offset) * vec3(0.5,1.0,0.5)
+          * vec3(c,1.0,c) + vec3(0,position.y/32.0-sin(time*32.0)*0.003,0);
+        gl_Position = projection * view * model * vec4(vpos,1);
+      }
+    `,
+    attributes: {
+      position: mesh.positions
+    },
+    uniforms: {
+      model: function (context) {
+        mat4.identity(model)
+        mat4.scale(model, model, [
+          1,1+(Math.sin(context.time*0.4)+Math.sin(context.time*2.1)*0.4)*0.03,1
+        ])
+        return model
+      },
+      time: regl.context('time')
+    },
+    elements: mesh.cells,
+    blend: {
+      enable: true,
+      func: {
+        src: 'src alpha',
+        dst: 'one minus src alpha'
+      }
+    },
+    depth: {
+      mask: false
+    }
+  })
+}
+function candle (regl) {
+  var model = []
+  var mesh = build(regl, 128, function (x,y,z) {
+    return cylinder(x,y,z, 0.15,0.7)
+  })
+  return regl({
+    frag: `
+      precision mediump float;
+      ${snoise}
+      varying vec3 vnorm, vpos;
+      uniform float time;
+      void main () {
+        float l = pow(dot(normalize(vec3(-0.8,0.4,0.3)),vnorm),1.5)
+          + snoise(vpos*0.1)*0.1
+          + abs(snoise(vpos*0.1+vec3(0,time*0.01,0))
+            *pow(vpos.y/32.0-1.0+0.2,8.0)+1.0)*0.5*0.2
+        ;
+        vec3 c = vec3(l,l,l) * vec3(0.9,0.4,0.4) * 0.8;
+        gl_FragColor = vec4(c,1);
+      }
+    `,
+    vert: `
+      precision mediump float;
+      ${snoise}
+      uniform mat4 projection, view, model;
+      uniform vec3 offset;
+      uniform float time;
+      attribute vec3 position, normal;
+      varying vec3 vpos, vnorm;
+      void main () {
+        vnorm = normal;
+        float h = pow(abs(position.y/32.0),3.0);
+        float dy = snoise(position*0.2+time*0.25)*0.2;
+        float dx = snoise(position*0.25+vec3(0,time*0.05,0)*2.0)*0.4 * h;
+        float dz = snoise(position*0.22+vec3(0,time*0.05,0)*2.0)*0.4 * h;
+        vpos = (position - offset) + vec3(dx,dy,dz);
+        gl_Position = projection * view * model * vec4(vpos,1);
+      }
+    `,
+    attributes: {
+      position: mesh.positions,
+      normal: normals(mesh.cells, mesh.positions)
+    },
+    uniforms: {
+      model: function () {
+        mat4.identity(model)
+        mat4.scale(model, model, [2/64,2/64,2/64])
+        mat4.translate(model, model, [-32,-32,-32])
+        return model
+      },
+      offset: [0,-20,0],
+      time: regl.context('time')
+    },
+    elements: mesh.cells
+  })
 }
 
 function base (regl) {
@@ -91,7 +207,7 @@ function base (regl) {
 }
 function candle (regl) {
   var model = []
-  var mesh = build(regl, 64, function (x,y,z) {
+  var mesh = build(regl, 128, function (x,y,z) {
     return cylinder(x,y,z, 0.15,0.7)
   })
   return regl({
@@ -99,9 +215,12 @@ function candle (regl) {
       precision mediump float;
       ${snoise}
       varying vec3 vnorm, vpos;
+      uniform float time;
       void main () {
         float l = pow(dot(normalize(vec3(-0.8,0.4,0.3)),vnorm),1.5)
           + snoise(vpos*0.1)*0.1
+          + abs(snoise(vpos*0.1+vec3(0,time*0.01,0))
+            *pow(vpos.y/32.0-1.0+0.2,8.0)+1.0)*0.5*0.2
         ;
         vec3 c = vec3(l,l,l) * vec3(0.9,0.4,0.4) * 0.8;
         gl_FragColor = vec4(c,1);
@@ -112,14 +231,15 @@ function candle (regl) {
       ${snoise}
       uniform mat4 projection, view, model;
       uniform vec3 offset;
+      uniform float time;
       attribute vec3 position, normal;
       varying vec3 vpos, vnorm;
       void main () {
         vnorm = normal;
         float h = pow(abs(position.y/32.0),3.0);
-        float dy = snoise(position*0.2)*0.2;
-        float dx = snoise(position*0.45)*0.2 * h;
-        float dz = snoise(position*0.42)*0.2 * h;
+        float dy = snoise(position*0.2+time*0.25)*0.2;
+        float dx = snoise(position*0.25+vec3(0,time*0.05,0)*2.0)*0.4 * h;
+        float dz = snoise(position*0.22+vec3(0,time*0.05,0)*2.0)*0.4 * h;
         vpos = (position - offset) + vec3(dx,dy,dz);
         gl_Position = projection * view * model * vec4(vpos,1);
       }
@@ -135,7 +255,8 @@ function candle (regl) {
         mat4.translate(model, model, [-32,-32,-32])
         return model
       },
-      offset: [0,-20,0]
+      offset: [0,-20,0],
+      time: regl.context('time')
     },
     elements: mesh.cells
   })
