9b48828061666fb04af8220eb32ddb934d3b64a1
substack <substack@9nTgtYmvW4HID6ayt6Icwc8WZxdifx5SlSKKIX/X/1g=.ed25519>
2016-09-25 16:27:18 -0400

    good enough

--- atmoblink/sfx.js
+++ atmoblink/sfx.js
@@ -18,11 +18,13 @@ module.exports = function (regl) {
         float s14 = (1.0-saw(1.0/4.0,t))*0.5;
         float s18 = (1.0-saw(1.0/8.0,t))*0.5;
         float s1 = (1.0-saw(1.0,t))*0.5;
+        float s2 = (1.0-saw(2.0,t))*0.5;
         float s4 = (1.0-saw(4.0,t))*0.5;
         float s8 = (1.0-saw(8.0,t))*0.5;
         return 0.0
           + clamp(sin_(sin_(100.0,t)+sin_(50.0,t),mod(t,1.0))
             * pow(s1,8.0)*4.0,-1.0,1.0) * 1.0
+            * step(mod(t,8.0),4.0)
           + clamp(sin_(sin_(200.0,t)+sin_(50.0,t),mod(t,1.0)+s4)
             * pow(s8,8.0)*1.0,-1.0,1.0) * 0.1
           + clamp(sin_(sin_(200.0,t)+sin_(50.0+sin_(10.0,t),mod(t,1.0)/4.0),s1)
@@ -34,9 +36,11 @@ module.exports = function (regl) {
             * pow(s14,4.0)*1.0,-1.0,1.0) * 0.2
           + clamp(sin_(sin_(400.0+sin_(10.0,t),mod(t,1.0))+sin_(100.0,t),mod(t,1.0))
             * pow(s18,8.0)*1.0,-1.0,1.0) * 0.2
+            * step(mod(t,16.0),12.0)
           + clamp(sin_(sin_(400.0,t)+sin_(200.0,t)+sin_(50.0,t),
-            mod(t,1.0)+sin_(0.25,t)*0.1)
-            * pow(max(s1,s4),8.0)*4.0,-1.0,1.0) * 0.1
+            mod(t,1.0)*0.5+sin_(0.25,t)*0.1)
+            * pow(max(s2,s8),8.0)*4.0,-1.0,1.0) * 0.05
+            * step(mod(t,16.0),12.0)
         ;
       }
     `,
