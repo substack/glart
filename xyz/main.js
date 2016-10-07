708b780ab40fbad19976913a6b29bfa099a52f06
substack <substack@9nTgtYmvW4HID6ayt6Icwc8WZxdifx5SlSKKIX/X/1g=.ed25519>
2016-10-06 23:58:41 -0400

    blurring

--- xyz/main.js
+++ xyz/main.js
@@ -6,19 +6,76 @@ var icosphere = require('icosphere')
 var mat4 = require('gl-mat4')
 var wireframe = require('screen-projected-lines')
 var glx = require('glslify')
+var xtend = require('xtend')
 
-var draw = { cool: cool(regl) }
+var ftex = regl.texture()
+var fb = regl.framebuffer({ color: ftex })
+var draw = {
+  blob: regl(blob(regl)),
+  blur: regl(blob(regl), { framebuffer: fb }),
+  feedback: feedback(regl)
+}
 regl.frame(function () {
-  regl.clear({ color: [0,0,0,1], depth: true })
+  draw.feedback()
   camera(function () {
-    draw.cool()
+    draw.blob()
+    ftex({ copy: true, mag: 'linear', min: 'linear' })
   })
 })
 
-function cool (regl) {
-  var model = []
-  var mesh = wireframe(icosphere(4))
+function feedback (regl) {
   return regl({
+    frag: `
+      precision mediump float;
+      uniform sampler2D texture;
+      uniform float time;
+      varying vec2 uv;
+      void main () {
+        float d = 0.001;
+        vec3 c = texture2D(texture,uv).rgb;
+        const int n = 4;
+        for (int i = 0; i < n; i++) {
+          c += texture2D(texture,uv+vec2(d,d)).rgb;
+          c += texture2D(texture,uv+vec2(-d,d)).rgb;
+          c += texture2D(texture,uv+vec2(d,-d)).rgb;
+          c += texture2D(texture,uv+vec2(-d,-d)).rgb;
+          d *= 2.0;
+        }
+        gl_FragColor = vec4(c*0.9/(1.0+float(n)*4.0),1);
+      }
+    `,
+    vert: `
+      precision mediump float;
+      attribute vec2 position;
+      varying vec2 uv;
+      void main () {
+        uv = (1.0+position)*0.5;
+        gl_Position = vec4(position,0,1);
+      }
+    `,
+    attributes: {
+      position: [-4,4,-4,-4,4,0]
+    },
+    uniforms: {
+      texture: ftex,
+      time: regl.context('time')
+    },
+    count: 3,
+    depth: { enable: false },
+    blend: {
+      enable: true,
+      func: {
+        src: 'src alpha',
+        dst: 'one minus src alpha'
+      }
+    }
+  })
+}
+
+function blob (regl) {
+  var model = []
+  var mesh = wireframe(icosphere(3))
+  return {
     frag: glx`
       precision mediump float;
       #pragma glslify: snoise = require('glsl-noise/simplex/4d')
@@ -62,8 +119,10 @@ function cool (regl) {
     elements: mesh.cells,
     uniforms: {
       model: function (context) {
+        var t = context.time*0.5
+        var r = t/2-Math.sin(2*t)/4
         mat4.identity(model)
-        mat4.rotateY(model,model,context.time*0.2)
+        mat4.rotateY(model,model,r)
         return model
       },
       time: regl.context('time'),
@@ -71,5 +130,5 @@ function cool (regl) {
         return context.viewportWidth / context.viewportHeight
       }
     }
-  })
+  }
 }
