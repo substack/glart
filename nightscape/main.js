5ef26c6da5e5ae538ebb9ac3300adc4ea274934a
substack <substack@9nTgtYmvW4HID6ayt6Icwc8WZxdifx5SlSKKIX/X/1g=.ed25519>
2017-01-06 07:48:44 -1000

    clouds

--- nightscape/main.js
+++ nightscape/main.js
@@ -17,7 +17,9 @@ var lights = (function () {
 })()
 
 var draw = {
-  box: box(regl)
+  box: box(regl),
+  train: train(regl),
+  cloud: cloud(regl)
 }
 var boxes = []
 for (var z = -16; z <= 16; z++) {
@@ -34,18 +36,132 @@ for (var z = -16; z <= 16; z++) {
   }
 }
 
+var clouds = []
+for (var i = 0; i < 400; i++) {
+  var theta = Math.random()*2*Math.PI
+  var r = Math.random()*100
+  clouds.push({
+    location: [
+      Math.cos(theta)*r,
+      5+Math.random()*2,
+      Math.sin(theta)*r,
+    ],
+    scale: [
+      1+Math.random()*8,
+      1+Math.random()*4,
+      1+Math.random()*8
+    ]
+  })
+}
+
 regl.frame(function (context) {
   var t = time = context.time
   regl.clear({ color: [0,0,0,1], depth: true })
   lights(function () {
     camera(function () {
       draw.box(boxes)
+      draw.train({
+        location: [(t*2+16)%32-16,0.05,-10],
+        scale: [0.05,0.05,0.05]
+      })
+      draw.cloud(clouds)
     })
   })
 })
 
 function xsin (x,n) {
   return Math.floor(Math.sin(x)*n)/n
+}
+
+function train (regl) {
+  var mesh = cube(1,[5,0.08,0.04])
+  var rmat = []
+  return regl({
+    frag: `
+      precision mediump float;
+      varying vec3 vpos, vnorm;
+      uniform vec3 location;
+      float xsin (float x, float n) {
+        return floor(sin(x)*n)*(1.0/n);
+      }
+      void main () {
+        vec3 v = location-vpos;
+        vec3 d0 = vec3(0.8,1,1) * xsin(0.0
+          + xsin(v.x*16.0,2.0)*4.0
+            * xsin((0.2-v.y*2.0)*2.0,8.0)*(0.5-v.y*4.0),
+          4.0)*2.0;
+        gl_FragColor = vec4(d0,1);
+      }
+    `,
+    vert: `
+      precision mediump float;
+      uniform mat4 projection, view;
+      attribute vec3 position, normal;
+      varying vec3 vpos, vnorm;
+      uniform vec3 location;
+      void main () {
+        vpos = position + location;
+        vnorm = normal;
+        gl_Position = projection * view * vec4(vpos,1.0);
+      }
+    `,
+    uniforms: {
+      location: regl.prop('location')
+    },
+    attributes: {
+      position: mesh.positions,
+      normal: anormals(mesh.cells, mesh.positions)
+    },
+    elements: mesh.cells
+  })
+}
+5
+function cloud (regl) {
+  var mesh = cube(1,[1,1,1])
+  var rmat = []
+  return regl({
+    frag: `
+      precision mediump float;
+      varying vec3 vpos, vnorm;
+      uniform vec3 location;
+      float xsin (float x, float n) {
+        return floor(sin(x)*n)*(1.0/n);
+      }
+      void main () {
+        vec3 v = location-vpos;
+        gl_FragColor = vec4(1,0.95,0.4,0.004);
+      }
+    `,
+    vert: `
+      precision mediump float;
+      uniform mat4 projection, view;
+      attribute vec3 position, normal;
+      varying vec3 vpos, vnorm;
+      uniform vec3 location, scale;
+      uniform float time;
+      void main () {
+        vpos = position*scale + location
+          + vec3(mod(time*0.2+location.x,180.0)-64.0,0,0);
+        vnorm = normal;
+        gl_Position = projection * view * vec4(vpos,1.0);
+      }
+    `,
+    uniforms: {
+      time: regl.context('time'),
+      location: regl.prop('location'),
+      scale: regl.prop('scale')
+    },
+    attributes: {
+      position: mesh.positions,
+      normal: anormals(mesh.cells, mesh.positions)
+    },
+    elements: mesh.cells,
+    blend: {
+      enable: true,
+      func: { src: 'src alpha', dst: 'one minus src alpha' }
+    },
+    depth: { mask: false }
+  })
 }
 
 function box (regl) {
@@ -62,7 +178,7 @@ function box (regl) {
       }
       void main () {
         vec3 N = normalize(vnorm-eye);
-        vec3 d0 = max(0.0,dot(-normalize(light0-vpos),N))*vec3(1,0.9,0.7)*0.5;
+        vec3 d0 = max(0.0,dot(-normalize(light0-vpos),N))*vec3(1,0.9,0.7)*0.3;
         float lights = max(0.0,xsin(xsin(vpos.y*32.0,4.0)
           * xsin(xsin(vpos.x*4.0+vpos.z*3.0,2.0)*19.0
             + vpos.x*11.0+vpos.z*17.0,2.0)
@@ -70,8 +186,8 @@ function box (regl) {
           * xsin(vpos.y*8.0+vpos.x+vpos.z,4.0),
           4.0)*2.0) * clamp((vpos.y*6.0-scale.y)/scale.y,0.0,1.0);
         vec3 d1 = lights*vec3(
-          (1.0+xsin(vpos.x*32.0+vpos.z*31.0,8.0))*0.3+0.5,
-          1, 1);
+          (1.0+xsin(time*0.2+vpos.x*32.0+vpos.z*31.0,8.0))*0.3+0.5,
+          1,1)*1.5*(1.0+xsin(time*2.0+vpos.x*vpos.x+vpos.y*vpos.y+vpos.z*vpos.z,4.0)*0.2);
         vec3 a = vec3(0.0,0.1,0.1)*0.5;
         gl_FragColor = vec4(pow(max(d0+d1,a),vec3(2.2)),1);
       }
