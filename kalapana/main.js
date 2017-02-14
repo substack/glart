8b3e062892e9ac6849660ad39af57d0b7a839a36
substack <substack@9nTgtYmvW4HID6ayt6Icwc8WZxdifx5SlSKKIX/X/1g=.ed25519>
2017-02-14 02:37:37 -1000

    water level

--- kalapana/main.js
+++ kalapana/main.js
@@ -5,16 +5,54 @@ var camera = require('regl-camera')(regl, {
 var glsl = require('glslify')
 var anormals = require('angle-normals')
 var icosphere = require('icosphere')
+var grid = require('grid-mesh')
 var draw = {
-  lava: lava(regl)
+  lava: lava(regl),
+  water: water(regl)
 }
-
 regl.frame(function () {
   regl.clear({ color: [0,0,0,1], depth: true })
   camera(function () {
     draw.lava()
+    draw.water()
   })
 })
+
+function water (regl) {
+  var size = 30
+  var mesh = grid(size,size,[-1,-1],[2/size,0],[0,2/size])
+  return regl({
+    frag: `
+      precision highp float;
+      varying vec3 vpos;
+      void main () {
+        float d = max(0.0,8.0-length(vpos.xz+vec2(0,-2)));
+        vec3 c = vec3(1,0.5,0)*pow(d*0.1,4.0)*0.5;
+        gl_FragColor = vec4(c,1);
+      }
+    `,
+    vert: glsl`
+      precision highp float;
+      #pragma glslify: snoise = require('glsl-noise/simplex/4d')
+      uniform mat4 projection, view;
+      uniform float time;
+      attribute vec2 position;
+      varying vec3 vpos;
+      void main () {
+        vpos = vec3(position.x*20.0,-2.0,position.y*20.0)
+          + vec3(0,snoise(vec4(position.x,0,position.y,time*0.5)),0);
+        gl_Position = projection * view * vec4(vpos,1);
+      }
+    `,
+    attributes: {
+      position: mesh.positions
+    },
+    elements: mesh.cells,
+    uniforms: {
+      time: regl.context('time')
+    }
+  })
+}
 
 function lava (regl) {
   var mesh = icosphere(3)
@@ -25,8 +63,9 @@ function lava (regl) {
       varying vec3 vnorm, vpos;
       uniform float time;
       void main () {
-        float d = pow((1.0+snoise(vec4(vpos+vec3(0,time*2.0,0),time*0.2)))*0.5
-          * (1.0+snoise(vec4(vpos*2.0+vec3(0,time*2.0,0),time*0.4)))*0.5,
+        float d = pow((1.0
+          + snoise(vec4(vpos*2.0+vec3(0,time*3.0,-time*0.5),time*0.8)))*0.5
+          * (1.0+snoise(vec4(vpos*4.0+vec3(0,time*3.0,-time*0.5),time*2.0)))*0.5,
           1.8);
         gl_FragColor = vec4(vec3(1,0.5,0)*(1.0-d),1);
       }
@@ -40,13 +79,14 @@ function lava (regl) {
       varying vec3 vpos, vnorm;
       void main () {
         vnorm = normal;
-        vec3 pos = clamp(vec3(-0.5),vec3(0.5),position);
+        vec3 pos = position;
         vec3 p = vec3(
           pos.x*0.8,
           pos.y*3.0+sin(pos.z*2.0+0.5)*0.5-0.5,
-          pos.z*0.2+cos(pos.y+0.5)*2.0-2.0
+          pos.z*0.4+cos(pos.y*pos.y*0.5+pos.y*0.5+0.5)*4.0-2.0
+            + pow(pos.y+0.5,2.0)
         );
-        vpos = p + vnorm * snoise(vec4(p*8.0,time)) * 0.05;
+        vpos = p + vnorm * snoise(vec4(p*1.0,time*2.0))*0.05;
         gl_Position = projection * view * vec4(vpos,1);
       }
     `,
